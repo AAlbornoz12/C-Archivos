aa3a37d104dbd2f59feac09631e2aa700b3d07b3
albornoz <albornoz_13-25@hotmail.com>
2017-11-21 10:39:26 +0100

    Archvos Actualizados

---
+++ 2017/elemplos/nombre.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char nombre[256];
+    printf("Dime tu nombre: ");
+    scanf(" %s", nombre);
+    printf("Hola. %s.\n",nombre);
+    return EXIT_SUCCESS;
+}
